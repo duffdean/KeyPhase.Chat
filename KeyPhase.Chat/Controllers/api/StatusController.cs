100f490ba8f33c78c63fc2afb15bdacb7daee6d6
Dean Duff <dean@bmc-ne.co.uk>
2018-04-14 18:26:43 +0100

    Working beta. With Cookies
    Working version, but using cookies, will be updating to use querystrings

---
+++ KeyPhase.Chat/Controllers/api/StatusController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KeyPhase.Chat.Controllers.api
+{
+    public class StatusController : ApiController
+    {
+        [System.Web.Mvc.AcceptVerbs("GET")]
+        [System.Web.Mvc.HttpGet]
+        public bool IsAlive()
+        {
+            return true;
+        }
+    }
+}
